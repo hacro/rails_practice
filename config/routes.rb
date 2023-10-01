049dc0fc2f7900a8712780205417c89b2b9379ed
hazuki <87082685+hacro@users.noreply.github.com>
2023-10-01 15:44:34 +0900

    dbからデータを取り出し、画面に表示させる。5章まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'index' => 'lists#index'
   get 'new' => 'lists#new'
   post 'lists' => 'lists#create'
-  get 'show' => 'lists#show'
+  get  'lists/:id' => 'lists#show', as: 'list'
   get 'it' => 'lists/it'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
