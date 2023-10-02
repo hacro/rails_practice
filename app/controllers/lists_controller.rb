24ecae31991bf2b6e96e1e06392cd91146867e89
hazuki <87082685+hacro@users.noreply.github.com>
2023-10-02 14:34:29 +0900

    第八章：バリデーションについて学習

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,11 +2,16 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
+
   def index
     @lists = List.all
   end
